4efa00b1ee55f9e943a96478ff78756b83f93a7a
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-17 05:03:12 +0530

    moved all files to src

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { config } from "./config/config";
+const port = config.server.port;
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {});
+// console.log(io);
+
+app.get("/", (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+
+let totalUsers = 0;
+let id = 0;
+
+io.on("connection", function (socket) {
+	console.log("A user connected");
+	totalUsers++;
+	console.log({ totalUsers });
+	id++;
+	const clientId = id;
+	socket.emit("new client", clientId);
+
+	socket.on("new message", (message: string) => {
+		// console.log(`Client_${clientId} says: ${message}`);
+		io.emit("new message", message);
+	});
+
+	//Whenever someone disconnects this piece of code executed
+	socket.on("disconnect", function () {
+		console.log("A user disconnected");
+		totalUsers--;
+		console.log({ totalUsers });
+	});
+});
+httpServer.listen(port, () => {
+	console.log(`server running on port: ${port}`);
+});
